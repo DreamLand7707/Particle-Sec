c3c0bd6bbb8cc374313d155a477c588a47bd0961
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2021-11-11 15:52:21 +0800

    update Main.c

--- Main.c
+++ Main.c
@@ -5,11 +5,11 @@ int main()
     float k,a,t1,t2,c;
     char d;
     int i,j=0;
-    printf("请输入第一个点的坐标，格式(x,y,z)，均为半角\n");
+    printf("锟斤拷锟斤拷锟斤拷锟揭伙拷锟斤拷锟斤拷锟斤拷锟疥，锟斤拷式(x,y,z)锟斤拷锟斤拷为锟斤拷锟絓n");
     scanf("%f,%f,%f",&A[0],&A[1],&A[2]);
-    printf("请输入第二个点的坐标，格式(x,y,z)，均为半角\n");
+    printf("锟斤拷锟斤拷锟斤拷诙锟斤拷锟斤拷锟斤拷锟斤拷锟疥，锟斤拷式(x,y,z)锟斤拷锟斤拷为锟斤拷锟絓n");
     scanf("%f,%f,%f",&B[0],&B[1],&B[2]);
-    printf("请输入你想要的高度a，单位为格数\n");
+    printf("锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷要锟侥高讹拷a锟斤拷锟斤拷位为锟斤拷锟斤拷\n");
     scanf("%f",&c);
     for(i=1;i<=3;i++,j++)
     {
@@ -20,14 +20,14 @@ int main()
     k=(A1[2]-B1[2])/(A1[0]-B1[0]);
     a=-c/(A1[0]*A1[0]);
     t1=A[0]-O[0];t2=B[0]-O[0];
-    printf("表达式如下:\nx=t\ny=%ft^2+%f\nz=%ft\n",a,c,k);
+    printf("锟斤拷锟斤拷式锟斤拷锟斤拷:\nx=t\ny=%ft^2+%f\nz=%ft\n",a,c,k);
     if(t1>t2)
     {
-        printf("t的取值范围为[%f,%f]\n",t2,t1);
+        printf("t锟斤拷取值锟斤拷围为[%f,%f]\n",t2,t1);
     }
     else
     {
-        printf("t的取值范围为[%f,%f]\n",t1,t2);
+        printf("t锟斤拷取值锟斤拷围为[%f,%f]\n",t1,t2);
     }
     getchar();
     getchar();
