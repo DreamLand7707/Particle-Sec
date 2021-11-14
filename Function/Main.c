e366dc75f4ad6ad7f1dc200851aea8457d982e83
DreamLand7707 <1753913274@qq.com>
2021-11-14 11:52:11 +0800

    11/14

--- Function/Main.c
+++ Function/Main.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main()
 {
+    printf("Cilck the \"Enter\" to begin\n");
+    getchar();
     char a,b,c,d;
-    printf("Please putin the function mode.\n");
-    printf("Now we have two choices:\"1&2\".\n");
+    printf("----Please putin the function mode.\n");
+    printf("---Now we have two choices:\"1&2\".\n");
+    printf("---List:\n   1 is linear function\n   2 is quadratic function\n");
+    printf("Click the char...\n");
     a=getchar();
     switch(a)
     {
@@ -11,8 +15,9 @@ int main()
         {
             char a1;
             extern void f11(void);
-            printf("Please putin the parameter mode.\n");
-            printf("We only have one mode:\"1\".\n");
+            printf("--Please putin the parameter mode.\n");
+            printf("-We only have one mode:\"1\".\n");
+            printf("Click the char...\n");
             getchar();
             a1=getchar();
             f11();
@@ -23,8 +28,10 @@ int main()
             char b1;
             extern void f21(void);
             extern void f22(void);
-            printf("Please putin the parameter mode.\n");
-            printf("We have two mode:\"1&2\".\n");
+            printf("--Please putin the parameter mode.\n");
+            printf("-We have two mode:\"1&2\".\n");
+            printf("-List:\n 1 is two dots and a height parameter\n 2 is three dots\n");
+            printf("Click the char...\n");
             getchar();
             b1=getchar();
             if(b1=='1')
